c48126fdbbdad2cab6afa13e8efb3a10f8642de3
renankanu <renankanu@gmail.com>
2022-03-25 07:57:10 -0300

    feature: add for lint

--- lib/app/core/database/database.dart
+++ lib/app/core/database/database.dart
@@ -1,1 +1,16 @@
-class Database {}
+import 'package:dotenv/dotenv.dart';
+import 'package:mysql1/mysql1.dart';
+
+class Database {
+  Future<MySqlConnection> openConnection() async {
+    return MySqlConnection.connect(
+      ConnectionSettings(
+        host: env['databaseHost'] ?? 'localhost',
+        port: int.tryParse(env['databasePort']!) ?? 3306,
+        user: env['databaseUser'],
+        password: env['databasePassword'],
+        db: env['databaseName'],
+      ),
+    );
+  }
+}
