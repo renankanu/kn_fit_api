2833c6fd38e5ca98d6347ed028070876a5ef3f11
Renan Kanu <renankanu@gmail.com>
2022-04-20 20:26:01 -0300

    feature - check if token is personal for add exercise

--- lib/app/modules/exercise/controller/exercise_controller.dart
+++ lib/app/modules/exercise/controller/exercise_controller.dart
@@ -22,6 +22,17 @@ class ExerciseController {
   Future<Response> saveExercise(Request request) async {
     return ResponseHelper.makeResponse(
       handlerResponse: () async {
+        final tokenType =
+            TokenTypeUtils.fromString(request.headers['referring_to']!);
+        if (tokenType == TokenType.student) {
+          return ResponseHelper.baseResponse(
+            403,
+            responseModel: ResponseModel(
+              data: null,
+              message: 'Acesso negado.',
+            ),
+          );
+        }
         final body = await request.readAsString();
         final exercise = ExerciseModel.requestMapping(body);
         await exerciseService.createExercise(exercise);
