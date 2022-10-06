61a8f379638128af9bb090ae0a98fe3bf40a7527
renankanu <renankanu@gmail.com>
2022-10-06 19:27:23 -0300

    refactor: change name personal training route

--- lib/app/modules/personal_training/personal_training_router.dart
+++ lib/app/modules/personal_training/personal_training_router.dart
@@ -10,6 +10,6 @@ class PersonalTrainingRouter implements IRouter {
     final personalTrainingController =
         GetIt.I.get<PersonalTrainingController>();
 
-    router.mount('/personalTraining', personalTrainingController.router);
+    router.mount('/personal-training', personalTrainingController.router);
   }
 }
